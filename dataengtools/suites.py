5d281b0e264099213a9bcd050b3f71102ac8a83c
Guilherme S. Magalhaes <silcol455@gmail.com>
2024-12-05 17:16:28 -0300

    Add .venv and test.py to .gitignore, and add PandasSuite class to suites.py

---
+++ dataengtools/suites.py
@@ -0,0 +1,15 @@
+
+import boto3
+from mypy_boto3_glue import GlueClient
+from mypy_boto3_s3 import S3Client
+from mypy_boto3_athena import AthenaClient
+from dataengtools.pandas_integration.catalog import GlueCatalogWithPandas
+
+class PandasSuite():
+    def get_catalog(self, 
+                    athena_s3_output_path: str, 
+                    glue_client: GlueClient = boto3.client('glue'), 
+                    s3_client: S3Client = boto3.client('s3'), 
+                    athena_client: AthenaClient = boto3.client('athena')
+    ):
+        return GlueCatalogWithPandas(glue_client, s3_client, athena_client, athena_s3_output_path)
