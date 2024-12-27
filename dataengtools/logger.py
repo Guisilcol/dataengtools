1f0c09f0b288709a0fcd117902b81b77cfae605a
Guilherme S. Magalhaes <silcol455@gmail.com>
2024-12-27 20:41:15 -0300

    Add console handler to logger

--- dataengtools/logger.py
+++ dataengtools/logger.py
@@ -1,5 +1,6 @@
 import logging
 from threading import Lock
+from sys import stdout
 
 class Logger(logging.Logger):
     _instance: 'Logger' = None
@@ -15,15 +16,13 @@ class Logger(logging.Logger):
     def _initialize_logger(self, logger_name: str) -> None:
         if not hasattr(self, '_initialized') or not self._initialized:
             super().__init__(logger_name)
-            handler = logging.StreamHandler()
+            # Add a console handler to the logger
+            handler = logging.StreamHandler(stdout)
             formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
             handler.setFormatter(formatter)
             self.addHandler(handler)
             self.setLevel(logging.DEBUG)
             self._initialized = True
-            
-    def info(self, msg: str) -> None:
-        print(f'INFO: {msg}')
 
     @classmethod
     def get_instance(cls, logger_name: str = "dataengtools_logger") -> 'Logger':
