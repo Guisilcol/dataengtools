adb893fe30971c03ee412d72a3b55eaeaf03fd5b
Guilherme Magalhaes <silcol455@gmail.com>
2025-01-09 18:07:24 -0300

    Refactor EngineFactory to use type hints and overloads for get_catalog_engine and get_filesystem_engine methods

--- dataengtools/engine_factory.py
+++ dataengtools/engine_factory.py
@@ -1,3 +1,5 @@
+from typing import TypeVar, Literal, overload
+import polars as pl
 import boto3
 from s3fs import S3FileSystem
 
@@ -9,32 +11,40 @@ from dataengtools.providers.aws.glue_catalog_metadata_handler import AWSGlueTabl
 from dataengtools.providers.aws.glue_catalog_partitions_handler import AWSGluePartitionHandler
 from dataengtools.providers.aws.s3_filesystem_handler import AWSS3FilesystemHandler
 
+T = TypeVar('T', pl.DataFrame, pl.LazyFrame)
+ProviderType = Literal['dataframe|aws', 'lazyframe|aws']
 
 class EngineFactory:
+   @overload
+   def get_catalog_engine(self, provider: Literal['dataframe|aws'], configuration: dict = {}) -> Catalog[pl.DataFrame]: ...
+   
+   @overload
+   def get_catalog_engine(self, provider: Literal['lazyframe|aws'], configuration: dict = {}) -> Catalog[pl.LazyFrame]: ...
+   
+   def get_catalog_engine(self, provider: ProviderType = 'dataframe|aws', configuration: dict = {}) -> Catalog[T]:
+       if provider == 'dataframe|aws':
+           glue_cli = configuration.get('glue_cli') or boto3.client('glue')
+           s3_cli = configuration.get('s3_cli') or boto3.client('s3')
+           s3fs = configuration.get('s3fs') or S3FileSystem()
+           
+           return PolarsDataFrameCatalog(
+               datatype_mapping=AWSGlueDataTypeToPolars(),
+               filesystem=AWSS3FilesystemHandler(s3fs),
+               partition_handler=AWSGluePartitionHandler(glue_cli, s3_cli),
+               table_metadata_retriver=AWSGlueTableMetadataRetriver(glue_cli)
+           )
+       
+       raise NotImplementedError(f'Catalog engine for provider {provider} is not implemented')
 
-    def get_catalog_engine(self, provider: str = 'dataframe|aws', configuration: dict = {}) -> Catalog:
-        if provider == 'dataframe|aws':
-            
-            glue_cli = configuration.get('glue_cli') or boto3.client('glue')
-            s3_cli = configuration.get('s3_cli') or boto3.client('s3')
-            s3fs = configuration.get('s3fs') or S3FileSystem()
-            
-            return PolarsDataFrameCatalog(
-                datatype_mapping=AWSGlueDataTypeToPolars(),
-                filesystem=AWSS3FilesystemHandler(s3fs),
-                partition_handler=AWSGluePartitionHandler(glue_cli, s3_cli),
-                table_metadata_retriver=AWSGlueTableMetadataRetriver(glue_cli)
-            )
-        
-        raise NotImplementedError(f'Catalog engine for provider {provider} is not implemented')
+   @overload
+   def get_filesystem_engine(self, provider: Literal['dataframe|aws'], configuration: dict = {}) -> Filesystem[pl.DataFrame]: ...
+   
+   @overload
+   def get_filesystem_engine(self, provider: Literal['lazyframe|aws'], configuration: dict = {}) -> Filesystem[pl.LazyFrame]: ...
 
-
-    def get_filesystem_engine(self, provider: str = 'dataframe|aws', configuration: dict = {}) -> Filesystem:
-        if provider == 'dataframe|aws':
-            s3fs = configuration.get('s3fs') or S3FileSystem()
-
-            return PolarsFilesystem(
-                handler=AWSS3FilesystemHandler(s3fs)
-            )
-        
-        raise NotImplementedError(f'Filesystem engine for provider {provider} is not implemented')
+   def get_filesystem_engine(self, provider: ProviderType = 'dataframe|aws', configuration: dict = {}) -> Filesystem[T]:
+       if provider == 'dataframe|aws':
+           s3fs = configuration.get('s3fs') or S3FileSystem()
+           return PolarsFilesystem(handler=AWSS3FilesystemHandler(s3fs))
+       
+       raise NotImplementedError(f'Filesystem engine for provider {provider} is not implemented')
