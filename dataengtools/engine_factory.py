91f7d5b7bd09835bc196144ef9a2fd9ddb0d03ec
Guilherme Magalhaes <silcol455@gmail.com>
2025-01-17 07:18:52 -0300

    Enhance EngineFactory to include detailed configuration documentation for catalog and filesystem engines

--- dataengtools/engine_factory.py
+++ dataengtools/engine_factory.py
@@ -3,6 +3,7 @@ import polars as pl
 import boto3
 from s3fs import S3FileSystem
 import duckdb
+from duckdb import DuckDBPyConnection
 
 from dataengtools.core.interfaces.engine_layer.catalog import CatalogEngine
 from dataengtools.core.interfaces.engine_layer.filesystem import FilesystemEngine
@@ -16,17 +17,30 @@ from dataengtools.providers.aws.glue_catalog_metadata_handler import AWSGlueTabl
 from dataengtools.providers.aws.glue_catalog_partitions_handler import AWSGluePartitionHandler
 from dataengtools.providers.aws.s3_filesystem_handler import AWSS3FilesystemHandler
 
-T = TypeVar('T', pl.DataFrame, pl.LazyFrame)
 ProviderType = Literal['dataframe|aws', 'lazyframe|aws']
 
 class EngineFactory:
-   @overload
-   def get_catalog_engine(self, provider: Literal['dataframe|aws'], configuration: dict = {}) -> CatalogEngine[pl.DataFrame]: ...
-   
-   @overload
-   def get_catalog_engine(self, provider: Literal['lazyframe|aws'], configuration: dict = {}) -> CatalogEngine[pl.LazyFrame]: ...
-   
-   def get_catalog_engine(self, provider: ProviderType, configuration: dict = {}) -> CatalogEngine[T]:
+    @overload
+    def get_catalog_engine(self, provider: Literal['dataframe|aws'], configuration: dict = {}) -> CatalogEngine[pl.DataFrame]:
+        """
+            Configuration is a dictionary that can contain the following keys:
+                - glue_cli: boto3.client('glue') instance
+                - s3_cli: boto3.client('s3') instance
+                - s3fs: s3fs.S3FileSystem instance
+        """
+        pass
+
+
+    @overload
+    def get_catalog_engine(self, provider: Literal['lazyframe|aws'], configuration: dict = {}) -> CatalogEngine[pl.LazyFrame]:
+        """
+            Configuration is a dictionary that can contain the following keys:
+                - glue_cli: boto3.client('glue') instance
+                - s3_cli: boto3.client('s3') instance
+                - s3fs: s3fs.S3FileSystem instance
+        """
+
+    def get_catalog_engine(self, provider: ProviderType, configuration: dict = {}) -> CatalogEngine:
         if provider == None:
             raise ValueError('Provider is required')
 
@@ -56,13 +70,30 @@ class EngineFactory:
         
         raise NotImplementedError(f'CatalogEngine engine for provider {provider} is not implemented')
 
-   @overload
-   def get_filesystem_engine(self, provider: Literal['dataframe|aws'], configuration: dict = {}) -> FilesystemEngine[pl.DataFrame]: ...
-   
-   @overload
-   def get_filesystem_engine(self, provider: Literal['lazyframe|aws'], configuration: dict = {}) -> FilesystemEngine[pl.LazyFrame]: ...
+    @overload
+    def get_filesystem_engine(self, provider: Literal['dataframe|aws'], configuration: dict = {}) -> FilesystemEngine[pl.DataFrame]: 
+        """
+        Configuration is a dictionary that can contain the following
+        keys:
+            - s3fs: s3fs.S3FileSystem instance
+        """
 
-   def get_filesystem_engine(self, provider: ProviderType, configuration: dict = {}) -> FilesystemEngine[T]:
+    @overload
+    def get_filesystem_engine(self, provider: Literal['lazyframe|aws'], configuration: dict = {}) -> FilesystemEngine[pl.LazyFrame]:
+        """
+        Configuration is a dictionary that can contain the following
+        keys:
+            - s3fs: s3fs.S3FileSystem instance
+        """    
+        pass
+    
+
+    def get_filesystem_engine(self, provider: ProviderType, configuration: dict = {}) -> FilesystemEngine:
+        """
+        Configuration is a dictionary that can contain the following
+        keys:
+            - s3fs: s3fs.S3FileSystem instance
+        """
         if provider == None:
             raise ValueError('Provider is required')
         
@@ -75,8 +106,17 @@ class EngineFactory:
             return PolarsLazyFrameFilesystem(handler=AWSS3FilesystemHandler(s3fs))
         
         raise NotImplementedError(f'FilesystemEngine engine for provider {provider} is not implemented')
-   
-   def get_sql_engine(self, provider: Literal['duckdb|aws'], configuration: dict = {}) -> SQLEngine:
+
+    def get_sql_engine(self, provider: Literal['duckdb|aws'], configuration: dict = {}) -> SQLEngine:
+        """
+        Configuration is a dictionary that can contain the following
+
+        keys:
+            - glue_cli: boto3.client('glue') instance
+            - connection: duckdb.DuckDBPyConnection instance
+            - database_metadata_retriever: DatabaseMetadataRetriever instance
+            - table_metadata_retriever: TableMetadataRetriver instance
+        """
         if provider == None:
             raise ValueError('Provider is required')
         
